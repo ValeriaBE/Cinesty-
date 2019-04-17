7b7d638445330025b6df4591045ea1fc01d654c9
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-04-17 09:38:42 -0500

    arreglando funcionalidad, se mezclo con la anterior version

--- src/main.js
+++ src/main.js
@@ -1,33 +1,10 @@
-let imagen = document.getElementById('imagen');
-let hi = document.getElementById('hi');
-let classicmov = document.getElementById('classicmov');
-let name = document.getElementById('name');
-let premiers = document.getElementById('premiers');
-const container = document.getElementById('container');
-const demo = document.getElementById('demo');
-container.style.display='none';
-const apicall = () => {
-    let randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
-    let randomMovie = randomMovieArray[randomNumber];
-    fetch('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&apikey=691b66e1')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            let image = data.Poster;
-            if (image !== 'N/A') {
-                imagen.src = image;
-            }
-            console.log(data.Year);
-            hi.innerHTML=data.Plot;
-            name.innerHTML=data.Title;
-        })
 let demo = document.getElementById('demo');
 const newMovies = document.getElementById('newMovies');
 const newBtn = document.getElementById('new');
 const classicbtn = document.getElementById('classic');
 const home = document.getElementById('home');
 const watch = document.getElementById('watch');
+let hi = document.getElementById('hi');
 const randomMovieArray = ['Toy Story 4', 'What men want', "Isn't it romantic",'The perfect Date', "Wonder Park", 'The lego movie 2', 'Shazam!', 'Happy Death day 2U','The upside', 'Drunk Parents', 'Stuber','The secret life of pets 2'];
 home.addEventListener('click', ()=>{
     hi.style.display='none';
@@ -124,4 +101,3 @@ const slide3 = sliderArr3.forEach(item =>{
         })
     })
 })
-};
