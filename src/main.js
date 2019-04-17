39e494f462273855fcac992bdcef5e2eeb4662e9
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-04-17 09:26:53 -0500

    agregando classical movies

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 let imagen = document.getElementById('imagen');
 let hi = document.getElementById('hi');
+let classicmov = document.getElementById('classicmov');
 let name = document.getElementById('name');
 let demo = document.getElementById('demo');
 const newMovies = document.getElementById('newMovies');
 const newBtn = document.getElementById('new');
+const classicbtn = document.getElementById('classic');
 const home = document.getElementById('home');
 const watch = document.getElementById('watch');
-const randomMovieArray = ['The Spongebob Movie', 'Deadpool', 'The Hangover', '21 Jump Street', 'The perfect Date', "There's something about Mary"];
+const randomMovieArray = ['Toy Story 4', 'What men want', "Isn't it romantic",'The perfect Date', "Wonder Park", 'The lego movie 2', 'Shazam!', 'Happy Death day 2U','The upside', 'Drunk Parents', 'Stuber','The secret life of pets 2'];
 home.addEventListener('click', ()=>{
     hi.style.display='none';
+    classicmov.style.display='none';
     demo.style.display='block';
     watch.style.display='block';
 })
@@ -32,9 +35,75 @@ const randomMovie = randomMovieArray.forEach(item => {
         .then(res => res.json())
         .then(data => {
             newBtn.addEventListener('click', () => {
+                classicmov.style.display='none';
                 demo.style.display = 'none';
                 watch.style.display = 'none';
+                hi.style.display='block';
                 document.getElementById('hi').innerHTML += card(data);
             });
         })
 });
+const classicMovieArray = ['Meet the Parents', 'The naked Gun', 'Mean girls', 'Pulp fiction', "There's something about Mary", 'Harold and Maude', 'Superbad', 'The mask', 'American Pie', 'School of rock', 'Old school', 'Slap Shot'];
+const classMovie = classicMovieArray.forEach(item => {
+    fetch('https://www.omdbapi.com/?t=' + item + '&apikey=691b66e1')
+        .then(res => res.json())
+        .then(data => {
+            classicbtn.addEventListener('click', () => {
+                demo.style.display = 'none';
+                watch.style.display = 'none';
+                hi.style.display='none';
+                classicmov.style.display='block';
+                document.getElementById('classicmov').innerHTML += card(data);
+            });
+        })
+});
+
+
+const pintarModal = (data) => {
+    return `
+        <div>
+            <h2>${data.Title}</h2>
+            <p>Actors: ${data.Actors}</p>
+            <p>Director: ${data.Director}</p>
+            <p>Plot: ${data.Plot}</p>
+        </div>`
+}
+
+const learnsponge = document.getElementById('learn1');
+const modalbody = document.getElementById('modal-body');
+const sliderArr = ['The spongebob movie'];
+const slide = sliderArr.forEach(item =>{
+    fetch('https://www.omdbapi.com/?t=' + item + '&apikey=691b66e1')
+    .then(res => res.json())
+    .then(data => {
+        learnsponge.addEventListener('click',()=> {
+            modalbody.innerHTML = pintarModal(data);
+        })
+    })
+});
+const learngrown = document.getElementById('learn2');
+const sliderArr2 = ['Grown-ups'];
+const slide2 = sliderArr2.forEach(item =>{
+    fetch('https://www.omdbapi.com/?t=' + item + '&apikey=691b66e1')
+    .then(res => res.json())
+    .then(data => {
+        learngrown.addEventListener('click',()=> {
+            modalbody.innerHTML = pintarModal(data);
+        })
+    })
+});
+
+const learnzoo = document.getElementById('learn3');
+const sliderArr3 = ['Zootopia'];
+const slide3 = sliderArr3.forEach(item =>{
+    fetch('https://www.omdbapi.com/?t=' + item + '&apikey=691b66e1')
+    .then(res => res.json())
+    .then(data => {
+        learnzoo.addEventListener('click',()=> {
+            modalbody.innerHTML = pintarModal(data);
+        })
+    })
+})
+
+
+
